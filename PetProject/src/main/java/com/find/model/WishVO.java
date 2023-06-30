4f598856d7668b2eb05dfed084e36587ed17426a
YeLim <min_yl8234@naver.com>
2023-06-30 13:55:08 +0900

    예림 커밋 3
    펫시터 찾기 페이지의 지역검색하여 해당 지역 보여주기, 찜 추가 ajax 구현 (로그인 기능 추가 후에 해야하는 작업이 있어서 완료단계는 아님)
    채팅 기능 뼈대 구현 - 웹소켓 이용해야 해서 pom.xml에 스프링 버전 수정, 관련 의존성 추가함. 혹시 모르니까 pull 후에 maven update 하기

--- PetProject/src/main/java/com/find/model/WishVO.java
+++ PetProject/src/main/java/com/find/model/WishVO.java
@@ -7,10 +7,11 @@ public class WishVO {
 	
 	private int wno;			// 찜목록 인덱스
 	
-	private String unickname;	// 이용자 닉네임
-	private String snickname;	// 찜한 펫시터 닉네임
+	private String unickname;	// 이용자 닉네임 - members_fk
+	private String snickname;	// 찜한 펫시터 닉네임 - members_fk
 	
-	private String ino;			// 펫시터 소개 번호?
+	private String ino;			// 펫시터 소개 번호? - introduce_fk
 	private String saddr;		// 펫시터 지역
 	private String title;		// 펫시터 소개 제목
+	private int wcheck;			// 찜 여부 체크
 }
